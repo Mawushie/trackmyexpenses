a29f31fb10ac35e74f08b36d791f29ce36cb8339
Mawushie <afi.mawushie.honyah@gmail.com>
2021-07-09 14:57:26 +0000

    routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter , Switch, Route} from 'react-router-dom';
 import './App.css';
-import HomePage from './components/HomePage';
-
+import Home from './pages/Home';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import HomePage from './components/HomePage'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path = '/' component = {Home} />
+          <Route exact path = '/signup' component = {SignUp} />
+          <Route exact path = '/login' component = {LogIn} />
+          <Route exact path = '/dashboard' component = {HomePage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
